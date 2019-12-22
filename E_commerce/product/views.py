236378fa51baa3938633e77bfe8956f0c99e33aa
kanishkshah <shahkanishk99@gmail.com>
2019-12-22 10:27:50 +0530

    Fixed permissions

--- E_commerce/product/views.py
+++ E_commerce/product/views.py
@@ -3,12 +3,14 @@ from rest_framework import viewsets
 from .models import Product,Cart
 from .serializers import ProductSerializer,CartSerializer
 from django.core.exceptions import PermissionDenied
-
+from .permissions import IsOwnerOrReadOnlyProduct,IsOwnerOrReadOnlyCompany
+from rest_framework import permissions
 # Create your views here.
 
 class ProductView(viewsets.ModelViewSet):
 	queryset = Product.objects.all()
 	serializer_class = ProductSerializer
+	permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnlyProduct]
 	def perform_create(self, serializer):
 		if(self.request.user.is_company):
 			serializer.save(publisher=self.request.user)
@@ -18,6 +20,7 @@ class ProductView(viewsets.ModelViewSet):
 class CartView(viewsets.ModelViewSet):
 	queryset = Cart.objects.all()
 	serializer_class = CartSerializer
+	permission_classes = [IsOwnerOrReadOnlyCompany]
 	def perform_create(self, serializer):
 		if(not self.request.user.is_company):
 			serializer.save(user=self.request.user)
